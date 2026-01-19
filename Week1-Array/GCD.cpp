01ada65359014b0d48fb30f94cf02fb8aadcdfd6
Gaurav132005 <gauravkkr345@gmail.com>
2026-01-20 00:53:16 +0530

    Understand the GCD concept here

---
+++ Week1-Array/GCD.cpp
@@ -0,0 +1,30 @@
+/**
+ *    author:  WhiteLion
+ *    created: 20
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+
+// GCD is use to calculate the greatest comman divisor max number which can divide both the numbers. we can also find the lcm
+// with the help of gcd formula lcm(a,b) = (a * b) / gcd(a,b);
+
+int gcd(int a, int b) {
+    if(b == 0) {
+        return a;
+    }
+
+    return gcd(b, a % b);
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int n, m;
+  cin >> n >> m;
+
+  int ans = gcd(n, m);
+
+  cout << ans << endl;
+  return 0;
+}

4e7d70d5f7828afc67779ef8d2393c27f1ab6323
Gaurav132005 <gauravkkr345@gmail.com>
2026-01-23 23:56:43 +0530

    Few Dp-Problem solved

---
+++ Week2-DP/RobberHouseI.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    
+
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+
+    for(int i = 0; i < n; i++) cin >> arr[i];
+
+    if(n == 1) {
+        cout << arr[0] << endl;
+        return 0;
+    }
+
+    if(n == 2) {
+        cout << max(arr[0], arr[1]) << endl;
+        return 0;
+    }
+
+    vector<int> dp(n, 0);
+    dp[0] = arr[0];
+    dp[1] = arr[1];
+
+    for(int i = 2; i < n; i++) {
+        dp[i] = max(dp[i - 1], dp[i - 2] + arr[i]);
+    }
+
+    cout << dp[n - 1] << endl;
+
+    return 0;
+}

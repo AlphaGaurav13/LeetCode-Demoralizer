bdfe634969e80a42d76fd4940a27ea6d6501eed4
Gaurav132005 <gauravkkr345@gmail.com>
2026-01-24 03:36:41 +0530

    Done

---
+++ Week2-DP/KnapSack/countSubSetSum.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    int sum;
+    cin >> sum;
+    vector<vector<int>> dp(n + 1, vector<int>(sum + 1, 0));
+
+    for(int i = 0; i <= n; i++) {
+        dp[i][0] = 1;
+    }
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= sum; j++) {
+            if(arr[i - 1] <= j) {
+                dp[i][j] = dp[i - 1][j] + dp[i - 1][j - arr[i - 1]];
+            }else {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+
+    cout << dp[n][sum] << endl;
+
+    return 0;
+}
